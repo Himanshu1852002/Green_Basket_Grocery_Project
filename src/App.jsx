43a5b134504d98750f3f1b3183ecf81c4c951239
Himanshu Vishwakarma <himanshu77jangid@gmail.com>
2024-11-30 14:18:58 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import Blog from './Pages/Blog/Blog';
 import Contact from './Pages/Contact/Contact';
 import Grocery from './Pages/Grocery/Grocery';
 import { useState } from 'react';
-import LoginPopup from './Components/Login_Popup/LoginPopup';
+import LoginPopup from './Components/Login_PopUp/LoginPopup';
 import Wishlist from './Pages/Wishlist/Wishlist';
 
 
