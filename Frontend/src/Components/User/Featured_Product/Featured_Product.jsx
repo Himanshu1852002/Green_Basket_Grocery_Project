9986dd6a624b0ef44053c919f075f5180082cab8
Himanshu Vishwakarma <himanshu77jangid@gmail.com>
2025-02-15 10:47:56 +0530

    Some changes

--- Frontend/src/Components/User/Featured_Product/Featured_Product.jsx
+++ Frontend/src/Components/User/Featured_Product/Featured_Product.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 import './Featured_Product.css';
 import apple_img from '../../../assets/Images/Images/apple.png';
 import strawberry from '../../../assets/Images/Images/Strawberry.png';
-import chocolate_img from '../../../assets/Images/Images/Chocolate.png';
-import popcorn_img from '../../../assets/Images/Images/popcorn,png.png';
-import tomato_img from '../../../assets/Images/Vegetables_Images/Tomato.png';
-import coca_cola_img from '../../../assets/Images/Images/coca-cola.png';
-import termaric_img from '../../../assets/Images/Images/termaric.png';
-import rice_img from '../../../assets/Images/Images/rice.png';
+import chocolateimg from '../../../assets/Images/Images/Chocolate.png';
+import popcornimg from '../../../assets/Images/Images/popcorn,png.png';
+import tomatoimg from '../../../assets/Images/Vegetables_Images/Tomato.png';
+import cocacolaimg from '../../../assets/Images/Images/coca-cola.png';
+import termaricimg from '../../../assets/Images/Images/termaric.png';
+import riceimg from '../../../assets/Images/Images/rice.png';
 import { useState } from 'react';
 
 
@@ -47,12 +47,12 @@ const Featured_Product = () => {
     const cards = [
         { imgSrc: apple_img, title: "Apple", description: "An apple a day keeps the doctor away is not just an old saying rooted  truth Apples are packed with fiber antioxidants and awealth of vitamins that support your overall health Their naturalsweetness makes them a satisfying snack while their low calorie count ensures they fit perfectly into any diet plan " },
         { imgSrc: strawberry, title: "Strawberry", description: "Strawberries are not only delicious but also packed with vitamin C and antioxidants.Their sweet and tangy flavor makes them a favorite in smoothies, desserts, or as a refreshing snack.Low in calories and high in fiber, strawberries offer numerous health benefits while satisfying your taste buds." },
-        { imgSrc: chocolate_img, title: "DairyMilk", description: "Indulge in the rich, velvety taste of chocolate—a timeless treat loved by all! Packed with antioxidants and a hint of sweetness, chocolate not only satisfies your cravings but also uplifts your mood. Whether it's a creamy milk chocolate bar or decadent dark chocolate, this delicacy is perfect for every occasion. A bite of joy in every piece!" },
-        { imgSrc: popcorn_img, title: "Popcorn", description: "Popcorn is a light and crunchy snack that is perfect for movie nights or anytime you need a quick, satisfying bite. High in fiber and low in calories, it's a healthier snack choice compared to other processed treats. Whether flavored with butter, cheese, or spices, popcorn is a versatile snack that everyone can enjoy." },
-        { imgSrc: tomato_img, title: "Tomato", description: "Tomatoes are a vibrant and versatile ingredient, packed with vitamins, minerals, and antioxidants. Whether fresh in salads, blended into sauces, or roasted to perfection, tomatoes add a burst of flavor and a healthy boost to any dish. Their rich red color is a sign of their nutritious value, making them an essential part of a balanced diet." },
-        { imgSrc: coca_cola_img, title: "Coca-Cola", description: "Quench your thirst with the fizzy delight of cold drinks! Perfect for hot days or lively gatherings, these refreshing beverages add a sparkling touch to any moment.From classic cola to fruity flavors, every sip is a burst of coolness and flavor that leaves you wanting more.Chill out and enjoy the effervescence" },
-        { imgSrc: termaric_img, title: "Termaric", description: "Turmeric, often referred to as 'golden spice,' is celebrated for its powerful anti-inflammatory and antioxidant properties. Used in cooking, medicine, and beauty treatments, turmeric adds a warm, earthy flavor to dishes and supports overall health. Its active compound, curcumin, is believed to promote healthy digestion, improve skin health, and boost immunity." },
-        { imgSrc: rice_img, title: "Rice", description: "Rice is a staple food in many cultures around the world. Packed with carbohydrates, it provides a quick energy boost. Whether white, brown, or wild, rice is a versatile ingredient used in countless dishes. Brown rice, in particular, offers more fiber and nutrients, making it a healthier option for those looking to maintain a balanced diet." }
+        { imgSrc: chocolateimg, title: "DairyMilk", description: "Indulge in the rich, velvety taste of chocolate—a timeless treat loved by all! Packed with antioxidants and a hint of sweetness, chocolate not only satisfies your cravings but also uplifts your mood. Whether it's a creamy milk chocolate bar or decadent dark chocolate, this delicacy is perfect for every occasion. A bite of joy in every piece!" },
+        { imgSrc: popcornimg, title: "Popcorn", description: "Popcorn is a light and crunchy snack that is perfect for movie nights or anytime you need a quick, satisfying bite. High in fiber and low in calories, it's a healthier snack choice compared to other processed treats. Whether flavored with butter, cheese, or spices, popcorn is a versatile snack that everyone can enjoy." },
+        { imgSrc: tomatoimg, title: "Tomato", description: "Tomatoes are a vibrant and versatile ingredient, packed with vitamins, minerals, and antioxidants. Whether fresh in salads, blended into sauces, or roasted to perfection, tomatoes add a burst of flavor and a healthy boost to any dish. Their rich red color is a sign of their nutritious value, making them an essential part of a balanced diet." },
+        { imgSrc: cocacolaimg, title: "Coca-Cola", description: "Quench your thirst with the fizzy delight of cold drinks! Perfect for hot days or lively gatherings, these refreshing beverages add a sparkling touch to any moment.From classic cola to fruity flavors, every sip is a burst of coolness and flavor that leaves you wanting more.Chill out and enjoy the effervescence" },
+        { imgSrc: termaricimg, title: "Termaric", description: "Turmeric, often referred to as 'golden spice,' is celebrated for its powerful anti-inflammatory and antioxidant properties. Used in cooking, medicine, and beauty treatments, turmeric adds a warm, earthy flavor to dishes and supports overall health. Its active compound, curcumin, is believed to promote healthy digestion, improve skin health, and boost immunity." },
+        { imgSrc: riceimg, title: "Rice", description: "Rice is a staple food in many cultures around the world. Packed with carbohydrates, it provides a quick energy boost. Whether white, brown, or wild, rice is a versatile ingredient used in countless dishes. Brown rice, in particular, offers more fiber and nutrients, making it a healthier option for those looking to maintain a balanced diet." }
     ];
     const handleSeeMore = (card) => {
         setPopupData(card);
