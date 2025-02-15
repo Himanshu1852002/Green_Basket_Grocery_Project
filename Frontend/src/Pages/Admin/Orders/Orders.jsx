3b6556c52616517e1103c41a981a31606974aa32
Himanshu Vishwakarma <himanshu77jangid@gmail.com>
2025-02-15 12:58:41 +0530

    Changes in admin order page

--- Frontend/src/Pages/Admin/Orders/Orders.jsx
+++ Frontend/src/Pages/Admin/Orders/Orders.jsx
@@ -6,8 +6,7 @@ import { FaArrowDown, FaArrowUp,} from "react-icons/fa";
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const [expandedOrder, setExpandedOrder] = useState(null); // Track the expanded order
-    const url = import.meta.env.VITE_API_BASE_URL;
-
+    const url = "https://green-basket-grocery-project.onrender.com"
     const fetchAllOrders = async () => {
         axios.get(`${url}/api/orders/fetchAllOrders`)
             .then((response) => {
